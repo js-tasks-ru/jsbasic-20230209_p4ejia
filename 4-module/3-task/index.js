0223c52181156ec01f85e7c7f89c5e67fa0e30f0
P4eJIa <p4ejia@gmail.com>
2023-03-04 15:01:33 +0530

    3-task 4-module completed

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,32 @@
 function highlight(table) {
-  // ваш код...
+  let arrTable = [];
+
+  for (let elem of table.rows[0].children) {
+    arrTable.push(elem.innerHTML);
+  }
+  
+  let status = arrTable.indexOf('Status');
+
+  for (let row of table.tBodies[0].rows) {
+  
+    if (row.cells[status].hasAttribute('data-available')) {
+      row.cells[status].dataset.available == 'true' ?
+      row.classList.add('available') :
+      row.classList.add('unavailable')
+    } else {
+    row.setAttribute('hidden', true);
+    }
+  
+    let age = arrTable.indexOf('Age');
+    if (row.cells[age].innerHTML < 18) {
+      row.style = 'text-decoration: line-through';
+    }
+    
+    let gender = arrTable.indexOf('Gender');
+    row.cells[gender].innerHTML == 'm' ?
+    row.classList.add('male') :
+    row.classList.add('female');
+
+  }
 }
+
