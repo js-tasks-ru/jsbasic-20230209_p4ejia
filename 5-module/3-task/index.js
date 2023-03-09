094f016a453787da617f8ebca8a57f368cddde0d
P4eJIa <p4ejia@gmail.com>
2023-03-09 19:10:03 +0530

    Удалил лишнее

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,33 +1,18 @@
 function initCarousel() {
   let position = 0;
-  // const container = document.querySelector('.container');
   const carouselInner = document.querySelector('.carousel__inner');
-  // const carouselSlide = document.querySelectorAll('.carousel__slide');
-  // const carouselOneSlide = document.querySelector('.carousel__slide');
-  // const carouselSlideWidth = carouselInner.offsetWidth;
-  // const containerWidth = container.offsetWidth;
-  // const carouselInnerWidth = carouselSlide.length * carouselSlideWidth - containerWidth;
 
   const btnPrev = document.querySelector('.carousel__arrow_left');
   const btnNext = document.querySelector('.carousel__arrow_right');
   
 btnPrev.onclick = function() {
-    // position += carouselSlideWidth;
     position -= 1;
-    // if (position > 0) {
-    //   position = 0;
-    // }
     carouselInner.style.transform = `translateX(-${carouselInner.offsetWidth * position}px)`;
     btns();
   };
 
   btnNext.onclick = function() {
-    // position -= carouselSlideWidth;
     position += 1;
-    // let carouselInnerWidth = carouselSlide.length * carouselInner.offsetWidth - container.offsetWidth;
-    // if (position < -carouselInnerWidth) {
-    //   position = -carouselInnerWidth;
-    // }
     carouselInner.style.transform = `translateX(-${carouselInner.offsetWidth * position}px)`;
     btns();
   };
