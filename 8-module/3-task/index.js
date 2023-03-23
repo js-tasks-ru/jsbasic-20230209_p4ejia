44842f0b7dbcd5c304b605a6eda21e2420976a09
P4eJIa <p4ejia@gmail.com>
2023-03-23 21:15:30 +0530

    3-task 8-module completed

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -1,31 +1,54 @@
 export default class Cart {
-  cartItems = []; // [product: {...}, count: N]
 
   constructor(cartIcon) {
+    this.cartItems = []; // [product: {...}, count: N]
     this.cartIcon = cartIcon;
   }
 
   addProduct(product) {
-    // ваш код
+    if (!product || typeof product !== 'object') {
+      return;
+    }
+    let cartItem = this.cartItems.find(item => item.product.id === product.id);
+
+    if (!cartItem) {
+      cartItem = { product: product, count: 1 };
+      this.cartItems.push(cartItem);
+
+    } else {
+      cartItem.count++;
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let cartItem = this.cartItems.find(item => item.product.id === productId);
+    if (cartItem) {
+      cartItem.count += amount;
+      if (cartItem.count === 0) {
+        this.cartItems.splice(this.cartItems.indexOf(cartItem), 1);
+      }
+      this.onProductUpdate(cartItem);
+    }
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((total, item) => total + item.count, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((total, item) => {
+      return total + item.product.price * item.count;
+    }, 0);
   }
 
   onProductUpdate(cartItem) {
+    console.log(`Product ${cartItem.id} updated in the cart`);
     // реализуем в следующей задаче
 
     this.cartIcon.update(this);
